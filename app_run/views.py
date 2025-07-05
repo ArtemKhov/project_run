9588b31707dfb5e293584f1b73b7a53e9da2e517
Artem <artem.kotyakhov@gmail.com>
2025-07-05 16:21:36 +0300

    add Runners info: serializer, viewset, route

--- app_run/views.py
+++ app_run/views.py
@@ -1,10 +1,11 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 
 from .models import Run
-from .serializers import RunSerializer
+from .serializers import RunSerializer, RunnerSerializer
 
 
 @api_view(['GET'])
@@ -20,3 +21,19 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class RunnerViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.filter(is_superuser=False)
+    serializer_class = RunnerSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        user_type = self.request.query_params.get('type', None)
+
+        if user_type == 'coach':
+            qs = qs.filter(is_staff=True)
+        elif user_type == 'athlete':
+            qs = qs.filter(is_staff=False)
+
+        return qs
--- project_run/urls.py
+++ project_run/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details, RunViewSet
+from app_run.views import company_details, RunViewSet, RunnerViewSet
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
+router.register('api/users', RunnerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
