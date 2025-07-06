39383a850958031a3276349ee9637efaf3cf2225
Artem <artem.kotyakhov@gmail.com>
2025-07-06 21:21:22 +0300

    remove debug_toolbar

--- project_run/settings/local.py
+++ project_run/settings/local.py
@@ -1,11 +1,7 @@
-import sys
-
 from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-INTERNAL_IPS = ["127.0.0.1"]
 
 
 DATABASES = {
@@ -14,14 +10,3 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-ENABLE_DEBUG_TOOLBAR = DEBUG and "test" not in sys.argv
-if ENABLE_DEBUG_TOOLBAR:
-    INSTALLED_APPS += [
-        "debug_toolbar",
-    ]
-    MIDDLEWARE += [
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ]
-    # Customize the config to support turbo and htmx boosting.
-    DEBUG_TOOLBAR_CONFIG = {"ROOT_TAG_EXTRA_ATTRS": "data-turbo-permanent hx-preserve"}
--- project_run/urls.py
+++ project_run/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -14,8 +13,3 @@ urlpatterns = [
     path('api/company_details/', company_details),
     path('', include(router.urls)),
 ]
-
-if settings.ENABLE_DEBUG_TOOLBAR:
-    urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),
-    ]
