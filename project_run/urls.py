9436ee7717eacddac333295f981df7d71f56ece4
Artem <artem.kotyakhov@gmail.com>
2025-07-22 17:15:51 +0300

    fix: change start/stop run urls

--- project_run/urls.py
+++ project_run/urls.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from app_run.views import company_details, RunViewSet, RunnerViewSet, StartRunViewSet, StopRunViewSet
+from app_run.views import company_details, RunViewSet, RunnerViewSet, StartRunAPIView, StopRunAPIView
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
-router.register('api/runs/<int:run_id>/start', StartRunViewSet, basename='run-start')
-router.register('api/runs/<int:run_id>/stop', StopRunViewSet, basename='run-stop')
 router.register('api/users', RunnerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
+    path('api/runs/<int:run_id>/start', StartRunAPIView.as_view()),
+    path('api/runs/<int:run_id>/stop', StopRunAPIView.as_view()),
     path('', include(router.urls)),
 ]
