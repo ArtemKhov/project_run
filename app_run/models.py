2d363a00490482c6006466dbfae33474b9c70d8f
Artem <artem.kotyakhov@gmail.com>
2025-07-22 17:17:52 +0300

    feat: add Status to Run model

--- app_run/models.py
+++ app_run/models.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Run(models.Model):
+    class Status(models.TextChoices):
+        INIT = 'init', 'Забег инициализирован'
+        IN_PROGRESS = 'in_progress', 'Забег начат'
+        FINISHED = 'finished', 'Забег закончен'
+
     created_at = models.DateTimeField(auto_now_add=True)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
+    status = models.CharField(max_length=50, choices=Status.choices, default=Status.INIT)
 
