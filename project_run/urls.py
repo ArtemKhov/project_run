b015ac116b725275746364f756e955d896cfe9a5
Artem <artem.kotyakhov@gmail.com>
2025-07-22 17:32:29 +0300

    fix: add slash to urls

--- project_run/urls.py
+++ project_run/urls.py
@@ -11,7 +11,7 @@ router.register('api/users', RunnerViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
-    path('api/runs/<int:run_id>/start', StartRunAPIView.as_view()),
-    path('api/runs/<int:run_id>/stop', StopRunAPIView.as_view()),
+    path('api/runs/<int:run_id>/start/', StartRunAPIView.as_view()),
+    path('api/runs/<int:run_id>/stop/', StopRunAPIView.as_view()),
     path('', include(router.urls)),
 ]
