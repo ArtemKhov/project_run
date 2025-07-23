f2704dd478fc95c73a6cab9ef980305f6d05caa2
Artem <artem.kotyakhov@gmail.com>
2025-07-23 15:02:31 +0300

    feat: add pagination to Runs API

--- app_run/views.py
+++ app_run/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
@@ -22,9 +23,15 @@ def company_details(request):
     return Response(details)
 
 
+class RunsPagination(PageNumberPagination):
+    page_size_query_param = 'size'
+    max_page_size = 50
+
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.select_related('athlete').all()
     serializer_class = RunSerializer
+    pagination_class = RunsPagination
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['status', 'athlete']
     ordering_fields = ['created_at']
