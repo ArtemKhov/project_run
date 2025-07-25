cb9dabed624c1188955b62afd3c9cf91b91c281d
Artem <artem.kotyakhov@gmail.com>
2025-07-25 18:23:44 +0300

    fix: change model and add migrations

--- app_run/models.py
+++ app_run/models.py
@@ -22,8 +22,9 @@ class Challenge(models.Model):
     full_name = models.CharField(max_length=255)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE, related_name='challenge')
 
+
 class Position(models.Model):
-    run = models.OneToOneField(Run, on_delete=models.CASCADE, related_name='position')
+    run = models.ForeignKey(Run, on_delete=models.CASCADE, related_name='position')
     latitude = models.DecimalField(max_digits=10, decimal_places=4)
     longitude = models.DecimalField(max_digits=10, decimal_places=4)
 
